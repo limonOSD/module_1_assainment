377680de62dba0523d5acad5b973efb7e3a1cfd5
limon <limonpaulargo47@gmail.com>
2023-11-07 13:51:30 +0600

    first commit

---
+++ bin/module_1_assainment.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 7;
+  int b = 3;
+
+  int addition = a + b;
+  int subtraction = a - b;
+  int multiplication = a * b;
+  double divission = a / b;
+  int modulus = a % b;
+  print('a=$a,b=$b');
+  print('Addition:$a+$b=$addition');
+  print('Subtraction:$a-$b=$subtraction');
+  print('Multiplication:$a*$b=$multiplication');
+  print('Divission=$a/$b=$divission');
+  print('Modulus:$a%$b=$modulus');
+}
